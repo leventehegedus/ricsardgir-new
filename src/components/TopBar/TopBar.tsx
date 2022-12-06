3b9139e791df82dc68e6b3ec06d694c3b8db1334
leventehegedus <levi.hegedus92@gmail.com>
2022-12-06 01:01:52 +0100

    icons update

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,12 +1,7 @@
 import { Link } from "react-router-dom";
 import { WavyLink } from "react-wavy-transitions";
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
-const routes = [
-  {
-    url: "https://facebook.com/ricsardgir",
-    favicon: "fa-brands fa-facebook"
-  },
-]
 
 interface TopBarProps {
   isMenuOpen: boolean,
@@ -15,22 +10,25 @@ interface TopBarProps {
 
 export const TopBar: React.FC<TopBarProps> = ({ isMenuOpen, setMenuOpen }) => {
   return (
-    <div className="sticky h-20 top-0 left-0 right-0 bg-black text-white z-50 flex items-center uppercase font-black">
-      <div onClick={() =>  { setMenuOpen(!isMenuOpen) }} className="w-1/4 h-full flex justify-center items-center cursor-pointer hover:text-gir-500 hover:bg-white/10">Menü</div>
-      <div className="w-1/2 flex justify-center h-full items-center hover:text-gir-500">
-        <WavyLink to={"/"} color="#ff0700" duration="1600" direction="up">
-          Ricsárdgír
-      </WavyLink>
-      </div>
-      <div className="w-1/4 flex justify-right h-full items-center">
-        {
-          routes.map((route, index) => {
-            return (
-              <a key={index} href={route.url} target="_blank">
-                {route.favicon}
-              </a>
-            )
-          })}
+    <div className="sticky h-20 top-0 left-0 right-0 bg-black text-white flex z-50 items-center uppercase font-black">
+      <div className="max-w-7xl flex m-auto items-center w-full px-4">
+        <div className="w-1/4 h-full flex justify-start items-center"><span className="cursor-pointer hover:text-gir-500" onClick={() => { setMenuOpen(!isMenuOpen) }}>Menü</span></div>
+        <div className="w-1/2 flex justify-center h-full items-center hover:text-gir-500">
+          <WavyLink to={"/"} color="#ff0700" duration="1600" direction="up">
+            Ricsárdgír
+          </WavyLink>
+        </div>
+        <div className="w-1/4 flex justify-end h-full items-center gap-4">
+          <a href="https://facebook.com/ricsardgir" target="_blank">
+            <FaFacebook size={"2em"} className="hover:text-gir-500"/>
+          </a>
+          <a href="https://instagram.com/ricsardgir" target="_blank">
+            <FaInstagram size={"2em"} className="hover:text-gir-500"/>
+          </a>
+          <a href="https://youtube.com/RHCDGR" target="_blank">
+            <FaYoutube size={"2em"} className="hover:text-gir-500"/>
+          </a>
+        </div>
       </div>
     </div>
   )
