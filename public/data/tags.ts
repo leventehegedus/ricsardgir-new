2c7f2169b5f521c919be2adc2c525140f5a36c9b
leventehegedus <levi.hegedus92@gmail.com>
2023-04-17 12:10:56 +0200

    data update

--- public/data/tags.ts
+++ public/data/tags.ts
@@ -1,5 +1,16 @@
 [
-    {"id": 1, "priority": 1, "tag": "koncert", "description": "szoktunk koncertezni, ilyen szokott lenni"},
-    {"id": 2, "priority": 1, "tag": "scooter", "description": "szeretjük a scootert, mert jó"},
-    {"id": 3, "priority": 1, "tag": "rólunk írták", "description": "azt írnak rólunk, amit akarnak, de azért jó lenne, ha jó lenne"}
+  {
+    "id": 1,
+    "tag": "koncert",
+    "description": "itt koncertekről tudsz olvasni, meg ilyenek"
+  },
+  {
+    "id": 2,
+    "tag": "scooter",
+    "description": "Kedvenc zenekarunk a Scooter"
+  }, {
+    "id": 3,
+    "tag": "rólunk írták",
+    "description": "írtak rólunk az újságokban meg az interneten"
+  }
 ]
